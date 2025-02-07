a202e150d929652cd0a420d29109847262737251
Pau Deu Esteller <pdeuu@Macbook-Air-de-Pau.local>
2025-02-07 09:37:37 +0100

    update main.js product/html #44

--- staff/pau-deu/product/app/main.js
+++ staff/pau-deu/product/app/main.js
@@ -1,27 +1,165 @@
-console.clear()
-
-console.log('Hello, App!')
+// component
+function Component(container) {
+    this.container = container
+}
 
 // landing
-var landing = document.createElement('div')
-document.body.appendChild(landing)
-var landingLogo = document.createElement('h1')
-landing.appendChild(landingLogo)
+var landing = new Component(document.createElement('div'))
+landing.mount = function () {
+    document.body.appendChild(this.container)
 
-//var landingLogoText = new Text('Logo')
-var landingLogoText = document.createTextNode('Logo')
-landingLogo.appendChild(landingLogoText)
+    var logo = document.createElement('h1')
+    logo.textContent = 'Logo'
+    this.container.appendChild(logo)
 
-// register
-var register = document.createElement('div')
-document.body.appendChild(register)
+    var registerAnchor = document.createElement('a')
+    registerAnchor.textContent = 'Register'
+    registerAnchor.addEventListener('click', function () {
+        document.body.removeChild(this.container)
+        document.body.appendChild(register.container)
+    }.bind(this))
+    this.container.appendChild(registerAnchor)
 
-// login
-var login = document.createElement('div')
-document.body.appendChild(login)
+    var orText = document.createTextNode('or')
+    this.container.appendChild(orText)
+    
+    var loginAnchor = document.createElement('a')
+    loginAnchor.textContent = 'Login'
+    loginAnchor.addEventListener('click', function () {
+        document.body.removeChild(this.container)
+        document.body.appendChild(login.container)
+    }.bind(this))
+    this.container.appendChild(loginAnchor)
+}
 
-// home
-var home = document.createElement('div')
-document.body.appendChild(home)
+/* register */
+var register = new Component(document.createElement('div'))
+register.mount = function () {
+    var logo = document.createElement('h1')
 
-// ...
+    logo.textContent = 'Logo'
+    this.container.appendChild(logo)
+
+
+    // form
+    var form = document.createElement('form')
+    this.container.appendChild(form)
+
+
+    // name
+    var nameLabel = document.createElement('label')
+    nameLabel.textContent = 'Name'
+    form.appendChild(nameLabel)
+
+    var nameInput = document.createElement('input')
+    form.appendChild(nameInput)
+
+
+    // email
+    var emailLabel = document.createElement('label')
+    emailLabel.textContent = 'E-mail'
+    form.appendChild(emailLabel)
+
+    var emailInput = document.createElement('input')
+    form.appendChild(emailInput)
+
+
+    // username
+    var usernameLabel = document.createElement('label')
+
+    usernameLabel.textContent = 'Username'
+    form.appendChild(usernameLabel)
+
+    var usernameInput = document.createElement('input')
+    form.appendChild(usernameInput)
+
+
+    // password
+    var passwordLabel = document.createElement('label')
+    passwordLabel.textContent = 'Password'
+    form.appendChild(passwordLabel)
+
+    var passwordInput = document.createElement('input')
+    passwordInput.type = 'password'  // Cambiado para que se vea como puntos
+    form.appendChild(passwordInput)
+
+
+    // submit
+    var submitButton = document.createElement('button')
+    submitButton.textContent = 'Register'
+    form.appendChild(submitButton)
+
+
+    // anchor
+    var loginAnchor = document.createElement('a')
+    loginAnchor.textContent = 'Login'
+    loginAnchor.addEventListener('click', function () {
+        document.body.removeChild(this.container)
+        document.body.appendChild(login.container)
+    }.bind(this))
+    this.container.appendChild(loginAnchor)
+}
+
+
+/* login */
+var login = new Component(document.createElement('div'))
+login.mount = function () {
+
+    var logo = document.createElement('h1')
+    logo.textContent = 'Logo'
+    this.container.appendChild(logo)
+
+
+    // form
+    var form = document.createElement('form')
+    this.container.appendChild(form)
+
+
+    // username
+    var usernameLabel = document.createElement('label')
+    usernameLabel.textContent = 'Username'
+    form.appendChild(usernameLabel)
+
+    var usernameInput = document.createElement('input')
+    form.appendChild(usernameInput)
+
+
+    // password
+    var passwordLabel = document.createElement('label')
+    passwordLabel.textContent = 'Password'
+    form.appendChild(passwordLabel)
+
+    var passwordInput = document.createElement('input')
+    passwordInput.type = 'password' 
+    form.appendChild(passwordInput)
+
+
+    // submit
+    var submitButton = document.createElement('button')
+    submitButton.textContent = 'Login'
+    form.appendChild(submitButton)
+
+
+    // anchor
+    var registerAnchor = document.createElement('a')
+    registerAnchor.textContent = 'Register'
+    registerAnchor.addEventListener('click', function () {
+        document.body.removeChild(this.container)
+        document.body.appendChild(register.container)
+    }.bind(this))
+    this.container.appendChild(registerAnchor)
+}
+
+
+/* home */
+var home = new Component(document.createElement('div'))
+home.mount = function () {
+    var logo = document.createElement('h1')
+    logo.textContent = 'Logo'
+    this.container.appendChild(logo)
+}
+
+landing.mount()
+register.mount()
+login.mount()
+home.mount()
