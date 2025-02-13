74bd83d85b8a1378b20f288882c153bd3028c8d6
Pau Deu Esteller <pdeuu@Macbook-Air-de-Pau.local>
2025-02-13 09:38:26 +0100

    add Landing.js #44

---
+++ staff/pau-deu/product/app/view/Landing.js
@@ -0,0 +1,24 @@
+function Landing() {
+    Component.call(this, 'div')
+    var logo = new Heading(1)
+    logo.setText('Logo')
+    this.add(logo)
+    var registerAnchor = new Anchor()
+    registerAnchor.setText('Register')
+    this.registerAnchor = registerAnchor
+    this.add(registerAnchor)
+    var orText = document.createTextNode('or')
+    this.container.appendChild(orText)
+    var loginAnchor = new Anchor()
+    loginAnchor.setText('Login')
+    this.loginAnchor = loginAnchor
+    this.add(loginAnchor)
+}
+Landing.prototype = Object.create(Component.prototype)
+Landing.prototype.constructor = Landing
+Landing.prototype.addRegisterClickListener = function (listener) {
+    this.registerAnchor.addClickListener(listener)
+}
+Landing.prototype.addLoginClickListener = function (listener) {
+    this.loginAnchor.addClickListener(listener)
+}
