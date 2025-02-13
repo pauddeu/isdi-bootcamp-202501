24ad359f8194d9798648703bb736d13359120352
Pau Deu Esteller <pdeuu@Macbook-Air-de-Pau.local>
2025-02-13 09:42:21 +0100

    update main.js  #44

--- staff/pau-deu/product/app/view/main.js
+++ staff/pau-deu/product/app/view/main.js
@@ -1,260 +1,32 @@
-console.clear()
 console.log('Hello, App!')
-
-// component
-function Component(tagName) {
-    this.container = document.createElement(tagName)
-}
-Component.prototype.add = function (child) {
-    this.container.appendChild(child.container)
-}
-Component.prototype.remove = function (child) {
-    this.container.removeChild(child.container)
-}
-Component.prototype.addClickListener = function (callback) {
-    this.container.addEventListener('click', callback)
-}
-function Form() {
-    Component.call(this, 'form')
-}
-Form.prototype = Object.create(Component.prototype)
-Form.prototype.constructor = Form
-Form.prototype.addSubmitListener = function (callback) {
-    this.container.addEventListener('submit', callback)
-}
-function Label() {
-    Component.call(this, 'label')
-}
-Label.prototype = Object.create(Component.prototype)
-Label.prototype.constructor = Label
-Label.prototype.setText = function (text) {
-    this.container.textContent = text
-}
-function Input() {
-    Component.call(this, 'input')
-}
-Input.prototype = Object.create(Component.prototype)
-Input.prototype.constructor = Input
-Input.prototype.setType = function (type) {
-    this.container.type = type
-}
-Input.prototype.setPlaceholder = function (placeholder) {
-    this.container.placeholder = placeholder
-}
-Input.prototype.getValue = function () {
-    return this.container.value
-}
-function Button() {
-    Component.call(this, 'button')
-}
-Button.prototype = Object.create(Component.prototype)
-Button.prototype.constructor = Button
-Button.prototype.setType = function (type) {
-    this.container.type = type
-}
-Button.prototype.setText = function (text) {
-    this.container.textContent = text
-}
-function Heading(level) {
-    Component.call(this, 'h' + level)
-}
-Heading.prototype = Object.create(Component.prototype)
-Heading.prototype.constructor = Heading
-Heading.prototype.setText = function (text) {
-    this.container.textContent = text
-}
-function Anchor() {
-    Component.call(this, 'a')
-}
-Anchor.prototype = Object.create(Component.prototype)
-Anchor.prototype.constructor = Anchor
-Anchor.prototype.setText = function (text) {
-    this.container.textContent = text
-}
-function Body() {
-    Component.call(this, 'body')
-}
-Body.prototype = Object.create(Component.prototype)
-Body.prototype.constructor = Body
-// body
-const body = new Body()
+var body = new Body()
 document.body = body.container
-// landing
-function Landing() {
-    Component.call(this, 'div')
-    
-    var logoContainer = new Component('div'); // Contenedor para logo y casita
-    logoContainer.container.style.display = 'flex'; // Usamos flexbox para alinearlos
-    logoContainer.container.style.alignItems = 'center'; // Centrar verticalmente
-
-    var homeIcon = new Anchor();
-    homeIcon.setText('🏠'); // Emoji de casita
-    homeIcon.addClickListener(function () {
-        body.remove(this)
-        body.add(landing)  // Volver al inicio (Landing)
-    }.bind(this))
-    logoContainer.add(homeIcon) // Agregar casita al lado del logo
-    
-    var logo = new Heading(1)
-    logo.setText('Logo')
-    logoContainer.add(logo)
-
-    this.add(logoContainer)
-    
-    var registerAnchor = new Anchor()
-    registerAnchor.setText('Register')
-    registerAnchor.addClickListener(function () {
-        body.remove(this)
-        body.add(register)
-    }.bind(this))
-    this.add(registerAnchor)
-    var orText = document.createTextNode(' or ')
-    this.container.appendChild(orText)
-    var loginAnchor = new Anchor()
-    loginAnchor.setText('Login')
-    loginAnchor.addClickListener(function () {
-        body.remove(this)
-        body.add(login)
-    }.bind(this))
-    this.add(loginAnchor)
-}
-Landing.prototype = Object.create(Component.prototype)
-Landing.prototype.constructor = Landing
 var landing = new Landing()
+landing.addRegisterClickListener(function () {
+    body.remove(landing)
+    body.add(register)
+})
+landing.addLoginClickListener(function () {
+    body.remove(landing)
+    body.add(login)
+})
 body.add(landing)
-/* register */
-function Register() {
-    Component.call(this, 'div')
-    var logo = new Heading(1)
-    logo.setText('Logo')
-    this.add(logo)
-    // form
-    var form = new Form()
-    form.addSubmitListener(function (event) {
-        event.preventDefault()
-        console.log('register submit')
-        var name = nameInput.getValue()
-        var email = emailInput.getValue()
-        var username = usernameInput.getValue()
-        var password = passwordInput.getValue()
-        console.log(name, email, username, password)
-    })
-    this.add(form)
-
-    // name
-    var nameLabel = new Label()
-    nameLabel.setText('Name')
-    form.add(nameLabel)
-    var nameInput = new Input()
-    form.add(nameInput)
-
-    // email
-    var emailLabel = new Label()
-    emailLabel.setText('E-mail')
-    form.add(emailLabel)
-    var emailInput = new Input()
-    emailInput.setType('email')
-    form.add(emailInput)
-
-    // username
-    var usernameLabel = new Label()
-    usernameLabel.setText('Username')
-    form.add(usernameLabel)
-    var usernameInput = new Input()
-    usernameInput.setType('text')
-    form.add(usernameInput)
-
-    // password
-    var passwordLabel = new Label()
-    passwordLabel.setText('Password')
-    form.add(passwordLabel)
-    var passwordInput = new Input()
-    passwordInput.setType('password')
-    form.add(passwordInput)
-
-    // submit
-    var submitButton = new Button()
-    submitButton.setText('Register')
-    submitButton.setType('submit')
-    form.add(submitButton)
-
-    // anchor
-    var loginAnchor = new Anchor()
-    loginAnchor.setText('Login')
-    loginAnchor.addClickListener(function () {
-        body.remove(this)
-        body.add(login)
-    }.bind(this))
-    this.add(loginAnchor)
-}
-Register.prototype = Object.create(Component.prototype)
-Register.prototype.constructor = Register
 var register = new Register()
-/* login */
-function Login() {
-    Component.call(this, 'div')
-    var logo = new Heading(1)
-    logo.setText('Logo')
-    this.add(logo)
-
-    // form
-    var form = new Form()
-    form.addSubmitListener(function (event) {
-        event.preventDefault()
-        console.log('login submit')
-        var username = usernameInput.getValue()
-        var password = passwordInput.getValue()
-        console.log(username, password)
-        body.remove(this)
-        body.add(home)
-    }.bind(this))
-    this.add(form)
-
-    // username
-    var usernameLabel = new Label()
-    usernameLabel.setText('Username')
-    form.add(usernameLabel)
-    var usernameInput = new Input()
-    usernameInput.setType('text')
-    form.add(usernameInput)
-
-    // password
-    var passwordLabel = new Label()
-    passwordLabel.setText('Password')
-    form.add(passwordLabel)
-    var passwordInput = new Input()
-    passwordInput.setType('password')
-    form.add(passwordInput)
-
-    // submit
-    var submitButton = new Button()
-    submitButton.setText('Login')
-    submitButton.setType('submit')
-    form.add(submitButton)
-
-    // anchor
-    var registerAnchor = new Anchor()
-    registerAnchor.setText('Register')
-    registerAnchor.addClickListener(function () {
-        body.remove(this)
-        body.add(register)
-    }.bind(this))
-    this.add(registerAnchor)
-}
-Login.prototype = Object.create(Component.prototype)
-Login.prototype.constructor = Login
+register.addLoginClickListener(function () {
+    body.remove(register)
+    body.add(login)
+})
+register.addRegisterSubmitListener(function () {
+    body.remove(register)
+    body.add(login)
+})
 var login = new Login()
-
-/* home */
-function Home() {
-    Component.call(this, 'div')
-    var logo = new Heading(1)
-    logo.setText('Logo')
-    this.add(logo)
-    var welcome = new Heading(2)
-    welcome.setText('Hello, World!')
-    this.add(welcome)
-}
-Home.prototype = Object.create(Component.prototype)
-Home.prototype.constructor = Home
+login.addRegisterClickListener(function () {
+    body.remove(login)
+    body.add(register)
+})
+login.addLoginSubmitListener(function () {
+    body.remove(login)
+    body.add(home)
+})
 var home = new Home()
